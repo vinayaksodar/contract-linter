1740b6d362594fbd80b4391523d42c812fff890f
vinayaksodar <vinayakmsodar@gmail.com>
2025-11-25 17:45:05 +0530

    initial template for word addin

---
+++ src/taskpane/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+Office.onReady(() => {
+  const rootEl = document.getElementById("root");
+  if (rootEl) {
+    createRoot(rootEl).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  }
+});
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import officeAddin from "vite-plugin-office-addin";
+import * as path from "path";
+import devCerts from "office-addin-dev-certs";
+
+async function getHttpsOptions() {
+  const httpsOptions = await devCerts.getHttpsServerOptions();
+  return {
+    ca: httpsOptions.ca,
+    key: httpsOptions.key,
+    cert: httpsOptions.cert,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ mode }) => ({
+  plugins: [
+    react(),
+    officeAddin({
+      devUrl: "https://localhost:3000",
+      prodUrl: "https://www.contoso.com", // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // '@/assets': path.resolve(__dirname, '../../../'),
+    },
+  },
+  root: "src",
+  build: {
+    rollupOptions: {
+      input: {
+        taskpane: "/taskpane/taskpane.html",
+        commands: "/commands/commands.html",
+      },
+    },
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  server:
+    mode !== "production" ? { https: await getHttpsOptions(), port: 3000 } : {},
+}));
